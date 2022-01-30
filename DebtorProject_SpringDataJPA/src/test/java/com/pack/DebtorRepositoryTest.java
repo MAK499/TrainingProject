064efd08a5a772dadec35363318d35c8e3ba6499
MAK499 <97936343+MAK499@users.noreply.github.com>
2022-01-30 20:38:50 +0530

    Updated Test Cases

--- DebtorProject_SpringDataJPA/src/test/java/com/pack/DebtorRepositoryTest.java
+++ DebtorProject_SpringDataJPA/src/test/java/com/pack/DebtorRepositoryTest.java
@@ -31,16 +31,17 @@ class DebtorRepositoryTest {
 	TransactionRepository transactionRepository;
 	
 	static DebtorDetails db;
-	static BankDetails bnk;
-	static TransactionDetails tx;
+	static BankDetails bnk = new BankDetails();
+	static TransactionDetails tx=new TransactionDetails(); ;
 	static int id;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception 
 	{
 		db= new DebtorDetails("Microsoft", "gachibowli", "Hyderabad", "a@microsoft.com", "04012345678", "9876543210");
-		//bnk = new BankDetails(id,"ICICI", "HiTech City", "ICIC1234567", "INR", "12345678901");
-		//tx = new TransactionDetails(id, 12345, "2022-01-29", "P", "Awaiting Approval From Admin");
+		bnk.setDebtorid(id);bnk.setBank_name("ICICI");bnk.setBranch_name("HiTech City");bnk.setIFSC("ICIC1234567");bnk.setCurrency("INR");bnk.setAccnum("12345678901");
+		tx.setDebtor_id(id);tx.setTransaction_num(12345);tx.setStatus("P");tx.setTransaction_date("2022-01-30");tx.setTransaction_info("Some Info");
+		
 		
 	
 	}
@@ -67,7 +68,7 @@ class DebtorRepositoryTest {
 		
 	}
 	
-	/*@Test
+	@Test
 	public void saveBankTest()
 	{
 		//bnk.setDebtorid(id);
@@ -88,8 +89,32 @@ class DebtorRepositoryTest {
 		Assertions.assertNotNull(tx.getTransaction_date());
 		Assertions.assertNotNull(tx.getStatus());
 		Assertions.assertNotNull(tx.getTransaction_info());
-	}*/
+	}
 	
+	@Test
+	public void PhoneNumTest()
+	{
+		Assertions.assertEquals(10,db.getPhonenum().length());
+	}
+	
+	@Test
+	public void IFSCTest()
+	{
+		Assertions.assertEquals(11,bnk.getIFSC().length());
+	}
+	
+	@Test
+	public void TransactionStatusTest()
+	{
+		if(tx.getStatus()=="A" || tx.getStatus()=="P" || tx.getStatus()=="R" )
+		{
+			Assertions.assertTrue(true);
+		}
+		else 
+		{
+			Assertions.assertTrue(false);
+		}
+	}
 	
 	
 
